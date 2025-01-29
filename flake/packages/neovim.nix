e744ed8bd94632b41d286f5964bcc7a52f8c8579
Ruowen Qin <chinqrw@gmail.com>
2025-01-29 11:13:34 +0800

    Fix patch
    Signed-off-by: Ruowen Qin <chinqrw@gmail.com>

--- flake/packages/neovim.nix
+++ flake/packages/neovim.nix
@@ -62,6 +62,9 @@ let
         };
       }).overrideAttrs
         (oa: {
+          # Disable patches applied by the nixpkgs tree-sitter derivation as they clash with this revision of TS.
+          # The equivalent patching is done below in `postPatch`
+          patches = [ ];
           postPatch = ''
             ${oa.postPatch}
             sed -e 's/playground::serve(.*$/println!("ERROR: web-ui is not available in this nixpkgs build; enable the webUISupport"); std::process::exit(1);/' \
